ac2d019f9c0f65d80c35817f576b679334b566bf
Noah Seo <sht6205@gmail.com>
2024-07-08 15:42:39 -0500

    Updated index&app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
 
 import { HashRouter as Router, Route } from 'react-router-dom';
@@ -10,13 +9,19 @@ import Support from '../Support/Support';
 import Understanding from '../Understanding/Understanding';
 
 function App() {
-
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Route exact path="/" component={Feeling} />
+        <Route path="/understanding" component={Understanding} />
+        <Route path="/support" component={Support} />
+        <Route path="/comments" component={Comments} />
+        <Route path="/review" component={Review} />
+      </Router>
     </div>
   );
 }
