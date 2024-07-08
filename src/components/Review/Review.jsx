4a65a62fa0c2aed4b2568ce07c3db74952eaa500
Noah Seo <sht6205@gmail.com>
2024-07-08 17:44:02 -0500

    Updated review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,12 +1,34 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
-function Reivew(){
+function Review() {
+  const feedback = useSelector(state => state.feedback);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    return (
-        <div>
-            <h1> Support</h1>
-        </div>
-    );
+  const handleSubmit = () => {
+    axios.post('/api/feedback', feedback)
+      .then(response => {
+        dispatch({ type: 'RESET_FEEDBACK' });
+        history.push('/');
+      })
+      .catch(error => {
+        console.error('Error Feedback', error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Review Your Feedback</h1>
+      <p>Feeling: {feedback.feeling}</p>
+      <p>Understanding: {feedback.understanding}</p>
+      <p>Support: {feedback.support}</p>
+      <p>Comments: {feedback.comments}</p>
+      <button data-testid="next" onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
-export default Reivew;
+
+export default Review;
